b71784499a421c4f4fde10e80981edc38a2a3a37
daipengyan <972781133@qq.com>
2019-01-23 10:25:41 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,22 +1,19 @@
-#include <iostream>  
-#include <memory>                          
-using namespace std;                              
-int main( ){   
-    int t,a[10];                                  
-    int i,j;
-    memset(a,0,sizeof(a));
-      for(i=0;i<10;i++){
-         cin>>a[i];}
-      for(j=1;j<=8;j++){
-          for(i=1;i<=9-j;i++){
-             if(a[i]<a[i+1]){
-                t=a[i];
-                a[i]=a[i+1];
-                a[i+1]=t;}
-          }
-       }
-       for(i=0;i<10;i++){
-         cout<<a[i]<<emdl;}
-                         
-        return 0;                                        
-}    
+#include<stdio.h>
+void m(int *s,int n){
+  int i,j,k=1,t;
+    for(i=0;i<n;i++)
+      for(j=n-1,k=0;j>i;j--)
+        if(s[j]>s[j-1]){
+          t=s[j];
+          s[j]=s[j-1];
+	      s[j-1]=t;
+	      k++;}
+}
+  int main(){
+    int a[10]={3,2,4,1,7,6,5,8,0,9},i;
+      m(a,10);
+        for(i=0;i<10;i++)
+           printf("%d  ",a[i]);
+    return 0;
+}
+
